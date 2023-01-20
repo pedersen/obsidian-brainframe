f75c62550f09c5fa537413476472ff9928f6345a
Michael J. Pedersen <m.pedersen@icelus.org>
2023-01-20 11:20:01 -0500

    Adding command, menu options, and settings to allow notes to be archived into a special folder.

--- main.ts
+++ main.ts
@@ -6,18 +6,20 @@ import {
 	Setting,
 	TFile,
 	TFolder,
-	request
+	request, TAbstractFile, Editor
 } from 'obsidian';
 import {clipboard} from 'electron';
 
 interface BrainframeSettings {
 	gitmarks: string;
 	products: string;
+	archived: string;
 }
 
 const DEFAULT_SETTINGS: BrainframeSettings = {
 	gitmarks: 'gitmarks.md',
-	products: 'products.md'
+	products: 'products.md',
+	archived: '_archived'
 }
 
 function validateUrl(url: string): URL {
@@ -62,7 +64,36 @@ export default class Brainframe extends Plugin {
 				}
 			}
 		);
+	}
 
+	async ensureArchiveExists() {
+		const archive_check = this.app.vault.getAbstractFileByPath(this.settings.archived);
+		const exists = (archive_check instanceof TFile) || (archive_check instanceof TFolder);
+		if ( !exists ) {
+			await this.app.vault.createFolder(this.settings.archived);
+		}
+		const archive = this.app.vault.getAbstractFileByPath(this.settings.archived);
+		if (!(archive instanceof TFolder)) {
+			new Notice('An error occurred creating your archive folder.')
+			throw new Error('Error during archive creation');
+		}
+	}
+
+	async archiveNote(toArchive: TAbstractFile|null) {
+		await this.ensureArchiveExists();
+		if (toArchive instanceof TFile) {
+			await this.app.vault.rename(toArchive, `${this.settings.archived}/${toArchive?.name}`)
+			await this.app.fileManager.processFrontMatter(toArchive, (frontMatter) => {
+				if (frontMatter.tags == null) {
+					frontMatter.tags = new Array<string>;
+				}
+				const tag = `#${this.settings.archived}`;
+				if (frontMatter.tags.indexOf(tag) == -1) {
+					frontMatter.tags.push(tag);
+				}
+			})
+			new Notice(`${toArchive.name} has been archived.`);
+		}
 	}
 
 	async onload() {
@@ -72,6 +103,41 @@ export default class Brainframe extends Plugin {
 			'Add Git bookmark');
 		await this.addMarksFile(this.settings.products, 'brainframe-add-productmark', 'p',
 			'Add Product bookmark');
+
+		this.addCommand({
+				id: 'brainframe-archive-note',
+				name: 'Archive Current Note',
+				editorCallback: (editor: Editor) => {
+					this.archiveNote(this.app.workspace.getActiveFile());
+				}
+			}
+		)
+
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("Archive Note")
+						.setIcon("document")
+						.onClick(async () => {
+							await this.archiveNote(file);
+						});
+				});
+			})
+		);
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("Archive current note")
+						.setIcon("document")
+						.onClick(async () => {
+							await this.archiveNote(view.file);
+						});
+				});
+			})
+		);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new BrainframeSettingTab(this.app, this));
@@ -130,6 +196,17 @@ class BrainframeSettingTab extends PluginSettingTab {
 					await this.plugin.saveSettings();
 				}));
 
+		new Setting(containerEl)
+			.setName('Archived notes folder:')
+			.setDesc('Location to store archived notes.')
+			.addText(text => text
+				.setPlaceholder('Enter the name of the archived notes folder')
+				.setValue(this.plugin.settings.archived)
+				.onChange(async (value:string) => {
+					console.log('archived notes name: ' + value);
+					this.plugin.settings.archived = value;
+					await this.plugin.saveSettings();
+				}))
 
 	}
 }
